d93432d35951b166e7e8a9cf4f9a33b5474c70ff
sarveshkadwe@gmail.com <sarveshkadwe@gmail.com>
2023-10-31 07:35:46 +0530

    Assignment03 Uploaded

---
+++ Assignment03/Assignmenr3_1/src/com/sunbeam/TestGenralStack.java
@@ -0,0 +1,15 @@
+public class TestGenralStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GenStack gst=new GenStack<>(2);
+		gst.push(10);
+		gst.push(20);
+		System.out.println(gst.pop());
+		System.out.println(gst.pop());
+
+
+	}
+
+}
