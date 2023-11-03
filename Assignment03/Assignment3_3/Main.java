5272dee25f0481a09ba0047369811403fe53820b
sarveshkadwe@gmail.com <sarveshkadwe@gmail.com>
2023-11-03 23:38:33 +0530

    Assignment 03

---
+++ Assignment03/Assignment3_3/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	K + L - M*N + (O^P) * W/U/V * T + Q
+	
+	prefix = ++-+KL*MN*//*^OPWUVTQ
+	postfix = KL+MN*-OP^W*U/V/T*+Q+
+	
+	
+	
+	
+	(A + B) * C - (D - E) * (F + G)
+	
+	prefix = -*+ABC*-DE+FG
+	postfix = AB+C*DE-FG+*-
+	
+	
+}
---
+++ Assignment03/Assignment3_4/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+	//Convert following expression to Prefix using stack algorithm.
+	5+9-4*(8-6/2)+1$(7-3)
+	Prefix = +-+59*4-8/62$1-73
+}
